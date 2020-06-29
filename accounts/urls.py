b59ed5bf002aec8842dc341399c8e20987072b3c
Darren Coppinger <darrencoppinger@gmail.com>
2020-06-29 15:34:03 +0000

    Create login and logout views and templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, include
-from accounts.views import index
+from accounts.views import login, logout
 
 urlpatterns = [
     url(r'^index/$', index, name='index'),
+    url(r'^logout/$', logout, name='logout'),
+    url(r'^login/$', login, name='login'),
 ]
