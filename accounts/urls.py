9f2967d8991b28e6aef1a9c2fdda618c102b8a36
Darren Coppinger <darrencoppinger@gmail.com>
2020-06-29 15:55:17 +0000

    Add navbar buttons

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from accounts.views import login, logout
 
 urlpatterns = [
-    url(r'^index/$', index, name='index'),
+    # url(r'^index/$', index, name='index'),
     url(r'^logout/$', logout, name='logout'),
     url(r'^login/$', login, name='login'),
 ]
