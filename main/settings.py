75173e031113933b66dcd87b2e3d4ad6b5e0c001
Darren Coppinger <darrencoppinger@gmail.com>
2020-06-29 16:16:33 +0000

    Include django-forms-bootstrap functionality

--- main/settings.py
+++ main/settings.py
@@ -42,7 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts'
+    'accounts',
+    'django_forms_bootstrap',
+
 ]
 
 MIDDLEWARE = [
