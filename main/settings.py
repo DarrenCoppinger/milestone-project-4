1f03fa630eccd0b521d33c5cb671e15f624f3211
Darren Coppinger <darrencoppinger@gmail.com>
2020-07-06 11:38:56 +0000

    Update requirements.txt for travis integration

--- main/settings.py
+++ main/settings.py
@@ -89,7 +89,7 @@ if "DATABASE_URL" in os.environ:
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
-    print("Postgres URL not found, using sqlite instead")
+    print("Postgres URL not found, using SQLite instead")
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
