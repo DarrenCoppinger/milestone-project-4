325fd42af4b3bb3328af057a9addb39ddff73107
Darren Coppinger <darrencoppinger@gmail.com>
2020-07-13 21:30:48 +0000

    Add plain text option for automatic booking emails

--- booking/admin.py
+++ booking/admin.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.template.loader import render_to_string
 from django.contrib.auth.models import User
 from django.contrib.sites.shortcuts import get_current_site
+from django.utils.html import strip_tags
 
 
 class ReservationAdmin(admin.ModelAdmin):
@@ -37,11 +38,12 @@ class ReservationAdmin(admin.ModelAdmin):
             from_email = settings.EMAIL_HOST_USER
             to_list = [obj.email, settings.EMAIL_HOST_USER]
             send_mail(
-                subject,
-                html_message,
-                from_email,
-                to_list,
-                fail_silently=False)
+                subject=subject,
+                message=strip_tags(html_message),
+                from_email=from_email,
+                recipient_list=to_list,
+                fail_silently=True,
+                html_message=html_message)
         return super().save_model(request, obj, form, change)
 
 
